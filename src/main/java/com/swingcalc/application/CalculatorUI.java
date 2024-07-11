fcdba2ad67694507caadecac24570349bf55317b
Jack <jackbadawy1999@gmail.com>
2024-07-11 17:57:56 +1000

    style

--- src/main/java/com/swingcalc/application/CalculatorUI.java
+++ src/main/java/com/swingcalc/application/CalculatorUI.java
@@ -17,13 +17,14 @@ public class CalculatorUI extends JPanel {
 
         displayText = new JTextArea();
         displayText.setBackground(Color.decode("#3c3c3c"));
+        displayText.setForeground(Color.decode("#ffffff"));
         displayText.setEditable(false);
         displayText.setFont(new Font("Arial", Font.PLAIN, 24));
         displayText.setPreferredSize(new Dimension(0, 50));
         add(displayText, BorderLayout.NORTH);
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(5, 4, 5, 5)); // Set layout to 5 rows and 4 columns
+        buttonPanel.setLayout(new GridLayout(5, 4, 5, 5)); 
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
         buttonPanel.setBackground(Color.decode("#1e1e1e"));
         numberButtons = new JButton[10];
@@ -68,12 +69,7 @@ public class CalculatorUI extends JPanel {
 
     private JButton createButton(String text, String color) {
         JButton button = new JButton(text) {
-            /**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			@Override
+            @Override
             protected void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g.create();
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -87,24 +83,26 @@ public class CalculatorUI extends JPanel {
             protected void paintBorder(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g.create();
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.setColor(getBackground());
+                g2.setColor(getBackground()); 
                 g2.draw(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 10, 10));
                 g2.dispose();
             }
             
             @Override
             public void setContentAreaFilled(boolean b) {
-              
+               
             }
         };
-        button.setPreferredSize(new Dimension(48, 40));
+        Dimension buttonSize = new Dimension(40, 40);
+        button.setPreferredSize(buttonSize);
+        button.setMinimumSize(buttonSize);
+        button.setMaximumSize(buttonSize);
         button.setBackground(Color.decode(color));
-        button.setForeground(Color.WHITE);
+        button.setForeground(text.equals("=") ? Color.decode("#2c2c2c") : Color.WHITE);
         button.setFocusPainted(false);
         button.setFont(new Font("Arial", Font.PLAIN, 20));
-        button.setOpaque(false);
+        button.setOpaque(false); 
         button.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); 
-       
         return button;
     }
 
